6a2667d783338b0d9abd30db5805d97ef6004715
Oluwafemi Olasbuomi <olasubomifemi98@gmail.com>
2021-07-01 08:43:06 +0100

    image upload to cloudinary

--- server/controllers/general.controller.js
+++ server/controllers/general.controller.js
@@ -1,5 +1,8 @@
 const codeBits = require("../lib/code.bits");
+const cloudinary = require('cloudinary').v2;
 
 exports.imageUpload = (req, res) => { 
-  return res.status(200).send({ url: req.file });
+  cloudinary.uploader.upload(req.file, async (error, result) => {
+    return res.status(200).send({ url: result.url });
+  });
 };
