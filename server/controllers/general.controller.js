d2b1ce5ac7b1d85005938c5af361cdc9e84a547d
Oluwafemi Olasbuomi <olasubomifemi98@gmail.com>
2021-07-01 08:48:10 +0100

    cool

--- server/controllers/general.controller.js
+++ server/controllers/general.controller.js
@@ -1,8 +1,13 @@
 const codeBits = require("../lib/code.bits");
 const cloudinary = require('cloudinary').v2;
-
-exports.imageUpload = (req, res) => { 
-  cloudinary.uploader.upload(req.file, async (error, result) => {
+cloudinary.config({
+  cloud_name: 'dhijlef1e',
+  api_key: '855759169965542',
+  api_secret: 'lKr2ekckuuf_N2vd-vbWtwPLnRA',
+  secure: true,
+});
+exports.imageUpload = (req, res) => {
+  cloudinary.uploader.upload(req.file.path, async (error, result) => {
     return res.status(200).send({ url: result.url });
   });
 };
